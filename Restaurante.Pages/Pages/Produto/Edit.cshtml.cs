c4e5d70fefefa2d360396256535be5524cc68e4d
Marco A. Schneiders <64050916+Dyest@users.noreply.github.com>
2023-05-05 17:32:34 -0300

    Add files via upload

--- Restaurante.Pages/Pages/Produto/Edit.cshtml.cs
+++ Restaurante.Pages/Pages/Produto/Edit.cshtml.cs
@@ -23,7 +23,7 @@ namespace Restaurante.Pages.Pages.Produto
             }
 
             var httpClient = new HttpClient();
-            var url = $"http://localhost:5171/Produto/Details/{id}";
+            var url = $"http://localhost:5085/Produto/Details/{id}";
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, url);
             var response = await httpClient.SendAsync(requestMessage);
 
